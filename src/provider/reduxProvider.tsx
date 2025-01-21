88476f9cba9e74a7a1de92ec6ae3d7e98e373488
james <james500nju@gmail.com>
2025-01-21 09:36:41 +0300

    redux

--- src/provider/reduxProvider.tsx
+++ src/provider/reduxProvider.tsx
@@ -1,17 +1,18 @@
 "use client"
-import { AppStore, Store } from '@/store/store'
-// import { AppStore, store } from '@/store/store'
-import React, { ReactNode, useRef} from 'react'
-import { Provider } from 'react-redux'
+import React, { ReactNode, useRef } from 'react';
+import { Provider } from 'react-redux';
+import { AppStore, Store } from '@/store/store';
 
 const ReduxProvider = ({ children }: { children: ReactNode }) => {
-    const storeRef = useRef<AppStore>()
+    // Initialize storeRef with undefined as the initial value
+    const storeRef = useRef<AppStore | undefined>(undefined);
+    
+    // Create the store instance if it's not already created
     if (!storeRef.current) {
-      // Create the store instance the first time this renders
-      storeRef.current = Store()
+        storeRef.current = Store();
     }
-  
-    return <Provider store={storeRef.current}>{children}</Provider>
-}
 
-export default ReduxProvider
+    return <Provider store={storeRef.current}>{children}</Provider>;
+};
+
+export default ReduxProvider;
