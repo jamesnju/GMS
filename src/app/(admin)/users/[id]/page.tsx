7175657008df2c84799290949f829f64ee2c7010
james <james500nju@gmail.com>
2025-01-31 17:55:40 +0300

    id fix

--- src/app/(admin)/users/[id]/page.tsx
+++ src/app/(admin)/users/[id]/page.tsx
@@ -3,11 +3,15 @@ import EditUser from "@/Globalcomponents/admin/EditUser";
 import React from "react";
 
 interface PageProps {
-  params: { id: number };
+  params: { id: string };
 }
 
 const Page = async ({ params }: PageProps) => {
-  const { id } =  params;
+  const id = Number(params.id); // Convert string to number
+
+  if (isNaN(id)) {
+    return <div>Invalid User ID</div>;
+  }
 
   const userData = await getUserById(id);
 
@@ -21,5 +25,4 @@ const Page = async ({ params }: PageProps) => {
     </div>
   );
 };
-
 export default Page;
